99134812918954d0ad8842f4a8e21c1cc1b937f5
Dominik Hrinkino <kurosudosk@gmail.com>
2021-08-25 21:30:15 +0200

    Created Class directly, Program.cs is now example of usage

---
+++ PlexAuth.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace PlexTokenAuth
+{
+    class PlexAuth
+    {
+
+        public static string Client_Identifier = null; // this is uniq ID of your application, you can write pretty much any number 
+        public static string X_Plex_Product = null; // Name of your application, also you will see this name in your plex account
+        private static string code = null;
+        private static string pin = null;
+        private static string token = null; 
+
+        public bool LoadPin(string filename)
+        {
+            try
+            {
+                string[] credentials = File.ReadAllLines(filename);
+                code = credentials[0];
+                pin = credentials[1];
+                token = credentials[2];
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+        private void genUrl(int countdown)
+        {
+            // open web browser
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://app.plex.tv/auth#?clientID=" + Client_Identifier + "&code=" + code + "&context%5Bdevice%5D%5Bproduct%5D=" + X_Plex_Product + "",
+                UseShellExecute = true
+            });
+            Thread.Sleep(countdown * 1000);
+        }
+
+        public void Set(string id, string name)
+        {
+            Client_Identifier = id;
+            X_Plex_Product = name;
+        }
+
+        public bool SavePin(string filename)
+        {
+            try
+            {
+                File.WriteAllLines(filename, new string[3] { code, pin, token });
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        public string Token()
+        {
+            string tkn;
+            string response = null;
+            using (WebClient webclient = new WebClient())
+            {
+                webclient.Headers["accept"] = "application/json";
+
+                string url = "https://plex.tv/api/v2/pins/" + pin + "/?code=" + code + "&X-Plex-Client-Identifier=" + Client_Identifier;
+                response = webclient.DownloadString("https://plex.tv/api/v2/pins/" + pin + "/?code=" + code + "&X-Plex-Client-Identifier=" + Client_Identifier);
+            }
+            dynamic obj_plex_pin = JsonConvert.DeserializeObject(response);
+            tkn = obj_plex_pin["authToken"];
+            token = tkn;
+            return token;
+        }
+
+        public void Generate()
+        {
+            string response = null;
+            using (WebClient webclient = new WebClient())
+            {
+                var data = new System.Collections.Specialized.NameValueCollection();
+                data.Add("strong", "true");
+                data.Add("X-Plex-Product", X_Plex_Product);
+                data.Add("X-Plex-Client-Identifier", Client_Identifier);
+                webclient.Headers["accept"] = "application/json";
+                byte[] resp_bytes = webclient.UploadValues("https://plex.tv/api/v2/pins/", "POST", data);
+                response = Encoding.UTF8.GetString(resp_bytes);
+            }
+            dynamic obj_plex_pin = JsonConvert.DeserializeObject(response);
+            code = obj_plex_pin["code"];
+            pin = obj_plex_pin["id"];
+            genUrl(15);
+        }
+        public string CurrentToken()
+        {
+            return token;
+        }
+        public bool ValidToken()
+        {
+            string response;
+            try
+            {
+                using (WebClient webclient = new WebClient()) { 
+                    webclient.Headers["accept"] = "application/json";
+                    string url = "https://plex.tv/api/v2/user?X-Plex-Product=" + X_Plex_Product + "&X-Plex-Client-Identifier=" + Client_Identifier + "&X-Plex-Token=" + token;
+                    response = webclient.DownloadString(url);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,94 +5,34 @@ using System.Threading;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.IO;
+
 namespace PlexTokenAuth
 {
     class Program
     {
 
-        public static string Client_Identifier = null; // this is uniq ID of your application, you can write pretty much any number 
-        public static string X_Plex_Product = null; // Name of your application, also you will see this name in your plex account
-        private static string code = null;
-        private static string pin = null;
         static void Main(string[] args)
         {
-            // setter
-            Set("123456789123456789123456789", "Name of your Application");
-
-            // get pin/id and code
-            pin_plex();
-            Thread.Sleep(2000);
-
-            // open web browser with out parameters
-            System.Diagnostics.Process.Start(new ProcessStartInfo
+            // example usage
+            PlexAuth PlexController = new PlexAuth();
+            PlexController.Set("123456789123456789123456789", "Name of your Application"); // ID of App and Name
+            
+            if (!PlexController.LoadPin("output.txt")) // if file do not exists generate new pin and token
             {
-                FileName = "https://app.plex.tv/auth#?clientID=" + Client_Identifier + "&code=" + code + "&context%5Bdevice%5D%5Bproduct%5D=" + X_Plex_Product + "",
-                UseShellExecute = true
-            });
-            
-            // wait time
-            Thread.Sleep(5000);
-            
-            // print token
-            Console.WriteLine(Get_Token());
+                PlexController.Generate(); // generate pin and code
+                PlexController.Token(); // generate token from pin and core
+                PlexController.SavePin("output.txt"); // save settings to output.txt
+            } else {
 
-            
-            // example of saving credentials
-            string[] credentials = { 
-            Client_Identifier, code, pin
-            };
-            File.WriteAllLines("credentials.txt", credentials);
-            
-            
-            // just for a demostration i'm cleaning variables
-            code = null;
-            Client_Identifier = null;
-            pin = null;
-            credentials = null;
-            credentials = File.ReadAllLines("credentials.txt");
-
-
-            // refill credentials from file
-            Client_Identifier = credentials[0];
-            code = credentials[1];
-            pin = credentials[2];
-            Console.WriteLine("New Token: ");
-            Console.WriteLine(Get_Token());
-
-        }
-        public static void Set(string id, string name) {
-            Client_Identifier = id;
-            X_Plex_Product = name;
-        }
-        private static string Get_Token()
-        {
-            string token;
-            string response = null;
-            using (WebClient webclient = new WebClient())
-            {
-                webclient.Headers["accept"] = "application/json";
-                response = webclient.DownloadString("https://plex.tv/api/v2/pins/"+pin+"/?code="+code+ "&X-Plex-Client-Identifier="+Client_Identifier);
+                if (!PlexController.ValidToken()) // check if existing token is not valid
+                {
+                    PlexController.Token(); // generate new token
+                    PlexController.SavePin("output.txt"); // save settings to output.txt
+                }
             }
-            dynamic obj_plex_pin = JsonConvert.DeserializeObject(response);
-            token = obj_plex_pin["authToken"];
-            return token;
+            Console.WriteLine(PlexController.CurrentToken()); // print current data
+
         }
-        private static void pin_plex()
-        {
-            string response = null;
-            using (WebClient webclient = new WebClient())
-            {
-                var data = new System.Collections.Specialized.NameValueCollection();
-                data.Add("strong", "true");
-                data.Add("X-Plex-Product", X_Plex_Product);
-                data.Add("X-Plex-Client-Identifier", Client_Identifier);
-                webclient.Headers["accept"] = "application/json";
-                byte[] resp_bytes = webclient.UploadValues("https://plex.tv/api/v2/pins/","POST", data);
-                response = Encoding.UTF8.GetString(resp_bytes);
-            }
-            dynamic obj_plex_pin = JsonConvert.DeserializeObject(response);
-            code = obj_plex_pin["code"];
-            pin = obj_plex_pin["id"];
-        }
+        
     }
 }
